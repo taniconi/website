8c093652c9b20666ba29f834ecc56615afd7b20e
Ramnico <117151788+taniconi@users.noreply.github.com>
2023-02-22 21:29:53 +0900

    Add files via upload

---
+++ ds/test/sw.js
@@ -0,0 +1,34 @@
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './audio-worklet.js',
+  './dark.css',
+  './icon.png',
+  './localforage.js',
+  './manifest.json',
+  './nds.js',
+  './nds.wasm',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
